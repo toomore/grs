f136917b9bc4768c58a0e426216a322c80e7ce03
Toomore Chiang <toomore0929@gmail.com>
2014-02-23 18:45:41 +0800

    Update OTCCLS.

--- tools/make_otc_list.py
+++ tools/make_otc_list.py
@@ -13,16 +13,7 @@ TWSEURL = 'http://www.gretai.org.tw/ch/stock/aftertrading/otc_quotes_no1430/stk_
         'mon': NOW.month,
         'day': NOW.day}
 
-TWSECLS = {'0049': u'封閉式基金',
-           '0099P': u'ETF',
-           '019919T': u'受益證券',
-           '0999': u'認購權證',  #(不含牛證)
-           '0999P': u'認售權證',  #(不含熊證)
-           '0999C': u'牛證',
-           '0999B': u'熊證',
-           '0999GA': u'附認股權特別股',
-           '0999GD': u'附認股權公司債',
-           '0999G9': u'認股權憑證',
+OTCCLS = {
            '01': u'水泥工業',
            '02': u'食品工業',
            '03': u'塑膠工業',
@@ -30,14 +21,22 @@ TWSECLS = {'0049': u'封閉式基金',
            '05': u'電機機械',
            '06': u'電器電纜',
            '07': u'化學生技醫療',
-           '21': u'化學工業',
-           '22': u'生技醫療業',
            '08': u'玻璃陶瓷',
            '09': u'造紙工業',
            '10': u'鋼鐵工業',
            '11': u'橡膠工業',
            '12': u'汽車工業',
            '13': u'電子工業',
+           '14': u'建材營造',
+           '15': u'航運業',
+           '16': u'觀光事業',
+           '17': u'金融保險',
+           '18': u'貿易百貨',
+           '19': u'綜合',
+           '20': u'其他',
+           '21': u'化學工業',
+           '22': u'生技醫療業',
+           '23': u'油電燃氣業',
            '24': u'半導體業',
            '25': u'電腦及週邊設備業',
            '26': u'光電業',
@@ -46,17 +45,18 @@ TWSECLS = {'0049': u'封閉式基金',
            '29': u'電子通路業',
            '30': u'資訊服務業',
            '31': u'其他電子業',
-           '14': u'建材營造',
-           '15': u'航運業',
-           '16': u'觀光事業',
-           '17': u'金融保險',
-           '18': u'貿易百貨',
-           '9299': u'存託憑證',
-           '23': u'油電燃氣業',
-           '19': u'綜合',
-           '20': u'其他',
-           'CB': u'可轉換公司債',}
-           #'ALL_1': u'全部'}
+           '32': u'文化創意業',
+           '80': u'管理股票',
+           'AA': u'受益證券',
+           'EE': u'上櫃指數股票型基金(ETF)',
+           'TD': u'台灣存託憑證(TDR)',
+           'WW': u'認購售權證',
+           'GG': u'認股權憑證',
+           'BC': u'牛證熊證',
+           #'EW': u'所有證券(不含權證、牛熊證)',
+           #'AL': u'所有證券 ',
+           #'OR': u'委託及成交資訊(16:05提供)',
+           }
 
 def fetch_otc_list():
     with open(SAVEPATH, 'w') as files:
@@ -64,7 +64,7 @@ def fetch_otc_list():
         re_pattern = re.compile(r'(=")?[\d\w]{4,6}(=)?')
         re_sub = re.compile(r'[^\w\d]')
 
-        for no in TWSECLS:
+        for no in OTCCLS:
             for i in csv.reader(urllib2.urlopen(TWSEURL % no).readlines()):
                 if len(i) >= 3 and re_pattern.match(i[0]):
                     pass
@@ -74,7 +74,7 @@ def fetch_otc_list():
                 if len(i) >= 2 and re_pattern.match(i[0]):
                     csv_file.writerow([re_sub.sub('', i[0]),
                                        i[1].decode('cp950').encode('utf-8'),
-                                       no, TWSECLS[no].encode('utf-8')])
+                                       no, OTCCLS[no].encode('utf-8')])
 
     with open(SAVEPATH, 'r') as files:
         csv_file = csv.reader(files)
@@ -92,8 +92,8 @@ def fetch_otc_list():
 def output_industry_code():
     with open('./industry_code.csv', 'w') as files:
         csv_file = csv.writer(files)
-        for i in sorted(TWSECLS):
-            csv_file.writerow([i, TWSECLS[i].encode('utf-8')])
+        for i in sorted(OTCCLS):
+            csv_file.writerow([i, OTCCLS[i].encode('utf-8')])
 
 if __name__ == '__main__':
     fetch_otc_list()
