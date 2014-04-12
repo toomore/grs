e0033f2b2a9668b565322f77f3267a35a8acc98f
Toomore Chiang <toomore0929@gmail.com>
2014-04-12 15:04:13 +0800

    Add error docs page.

--- grs/error.py
+++ grs/error.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''' grs Exception '''
+''' grs Exception '''
 # Copyright (c) 2012, 2013, 2014 Toomore Chiang, http://toomore.net/
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
