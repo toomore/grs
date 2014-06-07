227c6c60604a4bcc75ad772b3d7f90dd675817fa
Toomore Chiang <toomore0929@gmail.com>
2014-06-07 10:57:50 +0800

    Tiny changed.

--- grs/realtime2.py
+++ grs/realtime2.py
@@ -5,22 +5,22 @@ import urllib3
 from datetime import datetime
 
 URL = urllib3.connection_from_url('http://mis.tse.com.tw/',
-        headers={'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:30.0) Gecko/20100101 Firefox/30.0',
-                 #'Accept-Language': 'en-US'})
-                 'Accept-Language': 'zh-TW'})
+        #headers={'Accept-Language': 'en-US'})
+        headers={'Accept-Language': 'zh-TW'})
 
 #STOCKPATH = '/stock/api/getStockInfo.jsp?ex_ch=tse_1101.tw_20140530&json=1&delay=0&_=1401450118102'
-STOCKPATH = '/stock/api/getStockInfo.jsp?ex_ch=%(etype)s_%(no)s.tw_%(date)s&json=1&delay=0&_=%(timestamp)s'
+STOCKPATH = '/stock/api/getStockInfo.jsp?ex_ch=%(etype)s_%(no)s.tw_%(date)s&json=1&delay=%(delay)s&_=%(timestamp)s'
 
 
 class Realtime(object):
-    def __init__(self, no, date):
+    def __init__(self, no, date, delay=0):
         if not date:
             date = datetime.now()
 
         params = {'no': no, 'etype': self._etype,
                   'date': date.strftime('%Y%m%d'),
-                  'timestamp': int(time.time())}
+                  'timestamp': int(time.time()),
+                  'delay': delay}
 
         self.result = URL.request('GET', STOCKPATH % params)
         self.data = json.loads(self.result.data)
