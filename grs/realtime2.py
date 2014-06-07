c719ec6684ce77d4315d62877462fee69f8f5e87
Toomore Chiang <toomore0929@gmail.com>
2014-06-07 10:44:45 +0800

    Add realtime for OTC.

--- grs/realtime2.py
+++ grs/realtime2.py
@@ -12,6 +12,7 @@ URL = urllib3.connection_from_url('http://mis.tse.com.tw/',
 #STOCKPATH = '/stock/api/getStockInfo.jsp?ex_ch=tse_1101.tw_20140530&json=1&delay=0&_=1401450118102'
 STOCKPATH = '/stock/api/getStockInfo.jsp?ex_ch=%(etype)s_%(no)s.tw_%(date)s&json=1&delay=0&_=%(timestamp)s'
 
+
 class Realtime(object):
     def __init__(self, no, date):
         if not date:
@@ -24,11 +25,22 @@ class Realtime(object):
         self.result = URL.request('GET', STOCKPATH % params)
         self.data = json.loads(self.result.data)
 
-class RealtimeStock(Realtime):
+
+class RealtimeTESE(Realtime):
     _etype = 'tse'
 
     def __init__(self, no, date=None):
-        super(RealtimeStock, self).__init__(no, date)
+        super(RealtimeTESE, self).__init__(no, date)
+
+
+class RealtimeOTC(Realtime):
+    _etype = 'otc'
+
+    def __init__(self, no, date=None):
+        super(RealtimeOTC, self).__init__(no, date)
+
 
 if __name__ == '__main__':
-    print RealtimeStock(2618).data
+    from pprint import pprint
+    pprint(RealtimeTESE(2618, datetime(2014, 6, 5)).data)
+    pprint(RealtimeOTC(8446, datetime(2014, 6, 5)).data)
