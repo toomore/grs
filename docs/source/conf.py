3a7681da22bfe7b993aac7198618c6219c62c6bf
Toomore Chiang <toomore0929@gmail.com>
2015-03-19 23:06:32 +0800

    Update docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'grs'
-copyright = u'2014, Toomore Chiang'
+copyright = u'2014, 2015 Toomore Chiang'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
